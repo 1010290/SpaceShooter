9dc8b214ee1ba60d692d8f35addbadd70e82daa6
1010290 <s1010290@usls.edu.ph>
2025-12-30 17:19:04 +0800

    Health Bar Added

--- SpaceShooter/Assets/Scripts/UIController.cs
+++ SpaceShooter/Assets/Scripts/UIController.cs
@@ -3,12 +3,32 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    public Player playerController;
+    public Player shipController;
     public Slider healthBar;
+    public Image healthBarFill;
+    public Color[] fillColor = new Color[3];
 
     private void Start()
     {
+        //sets value of health bar to whole numbers
         healthBar.wholeNumbers = true;
+        //sets minimum value of healthbar to zero
         healthBar.minValue = 0;
+
+        fillColor[0] = new Color(0, 255, 0);
+        fillColor[1] = new Color(255, 194, 0);
+        fillColor[2] = new Color(255, 0, 0);
+    }
+
+    private void Update()
+    {
+        healthBar.maxValue = shipController.stats.maxHealth;
+        healthBar.value = shipController.stats.currentHealth;
+
+        healthBarFill.color = fillColor[0];
+        if(healthBar.value <= 80)
+            healthBarFill.color = fillColor[1];
+        if(healthBar.value <= 40)
+            healthBarFill.color = fillColor[2];
     }
 }
--- SpaceShooter/Assets/Space Shooter Template FREE/Scripts/Player.cs
+++ SpaceShooter/Assets/Space Shooter Template FREE/Scripts/Player.cs
@@ -7,6 +7,49 @@ using UnityEngine.UI;
 /// This script defines which sprite the 'Player" uses and its health.
 /// </summary>
 
+[System.Serializable]
+public class ShipStats
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+}
+
+public class Player : MonoBehaviour
+{
+    public ShipStats stats;
+    public GameObject destructionFX;
+
+    public static Player instance; 
+
+    private void Awake()
+    {
+        if (instance == null) 
+            instance = this;
+    }
+
+    private void Start()
+    {
+        Debug.Log(stats.maxHealth); //TESTING
+        stats.currentHealth = stats.maxHealth;
+    }
+
+    private void Update()
+    {
+        if (stats.currentHealth <= 0)
+        {
+            //destroys ship when health reaches zero
+            Instantiate(destructionFX, transform.position, Quaternion.identity); //generating destruction visual effect and destroying the 'Player' object
+            Destroy(gameObject);
+        }
+    }
+
+    public void GetDamage(int damage)   
+    {
+        stats.currentHealth -= 20;
+    }  
+}
+
+/*
 public class Player : MonoBehaviour
 {
     public GameObject destructionFX;
@@ -32,6 +75,7 @@ public class Player : MonoBehaviour
         Destroy(gameObject);
     }
 }
+*/
 
 
 
