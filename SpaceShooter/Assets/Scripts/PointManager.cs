eb2bec9c53631ed47f7b8cbe07af68b6dfb534f5
1010290 <s1010290@usls.edu.ph>
2026-01-22 13:28:57 +0800

    PlayerPrefs Score

--- SpaceShooter/Assets/Scripts/PointManager.cs
+++ SpaceShooter/Assets/Scripts/PointManager.cs
@@ -4,19 +4,32 @@ using TMPro;
 public class PointManager : MonoBehaviour, IDataPersistence
 {
     public int score;
+    public int highScore;
+
     //Reference to which text shows the score
     public TMP_Text scoreText;
+
+    //PREFS
+    public TMP_Text highestScoreText;
+    
+    //PREFS END
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         score = 0;
         scoreText.text  = "Score: " + score;
+        highestScoreText.text = "Highest: " + PlayerPrefs.GetInt("HighestScore", 0).ToString();
+    }
+
+    void Update()
+    {
+        UpdateHighScore();
     }
 
     public void LoadData(GameData data)
     {
-        this.score = data.highScore;
+        this.highScore = data.highScore;
     }
 
     public void SaveData(ref GameData data)
@@ -34,6 +47,23 @@ public class PointManager : MonoBehaviour, IDataPersistence
         if (score > 9980)
         {
             scoreText.text  = "Score: MAX";
+        }
+    }
+
+    public void UpdateHighScore()
+    {
+        //int highestScore = 0;
+
+        if(score > PlayerPrefs.GetInt("HighestScore", 0))
+        {
+            Debug.Log("Score is " + score);
+            Debug.Log("HighScore is " + PlayerPrefs.GetInt("HighestScore", 0));
+            Debug.Log("Changed Score");
+            PlayerPrefs.SetInt("HighestScore", score);
+            highestScoreText.text = "Highest: " + score.ToString();
+        } else
+        {
+            Debug.Log("Didn't Change Score");
         }
     }
 }
